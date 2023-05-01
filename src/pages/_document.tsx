ceca2b964404ae3ac9114bcafb9551adcf42775e
Syahril Anwar <sagaming97@gmail.com>
2023-05-01 17:57:10 +0800

    update: replace assets

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="icon"
+          href="https://img.icons8.com/ios-glyphs/30/000000/github.png"
+          type="image/x-icon"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
